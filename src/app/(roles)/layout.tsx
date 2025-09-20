fed4e26ae35e4358bdcc82b3d59a71d062af2778
Devarsh Shah <devarshshah0804@gmail.com>
2025-09-20 19:23:39 +0000

    add user profile on navbar

--- src/app/(roles)/layout.tsx
+++ src/app/(roles)/layout.tsx
@@ -1,4 +1,5 @@
 import { MainNav } from "./components/MainNav";
+import { UserProfile } from "./components/UserProfile";
 
 export default function RolesLayout({
     children,
@@ -8,8 +9,9 @@ export default function RolesLayout({
     return (
         <div className="flex-col md:flex">
             <div className="border-b">
-                <div className="flex h-16 items-center px-4 container mx-auto">
+                <div className="flex h-16 items-center justify-between px-4 container mx-auto">
                     <MainNav />
+                    <UserProfile />
                 </div>
             </div>
             <div className="flex-1 space-y-4 p-8 pt-6">
